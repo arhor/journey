5f0938bdef4295561f94d9b0a4a44b23d25a5bc8
Maksim Buryshynets <maksim.buryshynets@noom.com>
2026-03-05 11:48:57 +0100

    Move `AppCoroutineScope` qualifier to a separate file.

--- app/src/main/kotlin/com/github/arhor/journey/di/AppInfrastructureModule.kt
+++ app/src/main/kotlin/com/github/arhor/journey/di/AppInfrastructureModule.kt
@@ -8,12 +8,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.serialization.json.Json
-import javax.inject.Qualifier
 import javax.inject.Singleton
-
-@Qualifier
-@Retention(AnnotationRetention.BINARY)
-annotation class AppCoroutineScope
 
 @Module
 @InstallIn(SingletonComponent::class)
