1292b17559230217fae6acccba019d0f0315f487
Maksim Buryshynets <maksim.buryshynets@noom.com>
2026-05-08 14:25:36 +0200

    Replace manual pi constant with `std::numbers::pi` in `WebMercator.cpp`.
    - Include the `<numbers>` header to access standard mathematical constants.
    - Remove the locally defined `kPi` constant.
    - Update the calculation for `kDegreesToRadians` and the `latitudeToMercatorY` function to use `std::numbers::pi`.

--- app/src/main/cpp/lib/geo/WebMercator.cpp
+++ app/src/main/cpp/lib/geo/WebMercator.cpp
@@ -1,13 +1,13 @@
 #include "WebMercator.hpp"
 
 #include <cmath>
+#include <numbers>
 
 namespace custom_map_layers::geo {
 namespace {
 
 constexpr double kEarthCircumferenceMeters = 40075016.68557849;
-constexpr double kPi = 3.14159265358979323846264338327950288;
-constexpr double kDegreesToRadians = kPi / 180.0;
+constexpr double kDegreesToRadians = std::numbers::pi / 180.0;
 
 }  // namespace
 
@@ -17,7 +17,7 @@ double longitudeToMercatorX(double longitude) {
 
 double latitudeToMercatorY(double latitude) {
     const double radians = latitude * kDegreesToRadians;
-    return (1.0 - std::log(std::tan(radians) + (1.0 / std::cos(radians))) / kPi) / 2.0;
+    return (1.0 - std::log(std::tan(radians) + (1.0 / std::cos(radians))) / std::numbers::pi) / 2.0;
 }
 
 double metersToMercatorUnits(double meters, double latitude) {
