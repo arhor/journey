09867ced260ebed4378bfdb4d40be75350f5aea7
Maksim Buryshynets <maksim.buryshynets@noom.com>
2026-05-08 13:38:57 +0200

    refactor: remove obsolete native NDC projection helpers

--- app/src/main/cpp/lib/geo/WebMercator.cpp
+++ app/src/main/cpp/lib/geo/WebMercator.cpp
@@ -6,14 +6,8 @@ namespace custom_map_layers::geo {
 namespace {
 
 constexpr double kEarthCircumferenceMeters = 40075016.68557849;
-constexpr double kTileSize = 512.0;
 constexpr double kPi = 3.14159265358979323846264338327950288;
-constexpr double kFullCircleRadians = 2.0 * kPi;
 constexpr double kDegreesToRadians = kPi / 180.0;
-
-double degreesToRadiansIfNeeded(double value) {
-    return std::abs(value) > kFullCircleRadians ? value * kDegreesToRadians : value;
-}
 
 }  // namespace
 
@@ -29,76 +23,6 @@ double latitudeToMercatorY(double latitude) {
 double metersToMercatorUnits(double meters, double latitude) {
     const double radians = latitude * kDegreesToRadians;
     return meters / (kEarthCircumferenceMeters * std::cos(radians));
-}
-
-ScreenPoint projectToNdc(
-        double longitude,
-        double latitude,
-        double altitudeMeters,
-        const mbgl::style::CustomLayerRenderParameters& params
-) {
-    const double worldSize = kTileSize * std::pow(2.0, params.zoom);
-
-    const double cameraX = longitudeToMercatorX(params.longitude) * worldSize;
-    const double cameraY = latitudeToMercatorY(params.latitude) * worldSize;
-
-    const double pointX = longitudeToMercatorX(longitude) * worldSize;
-    const double pointY = latitudeToMercatorY(latitude) * worldSize;
-
-    const double altitudePixels = altitudeMeters * metersToMercatorUnits(1.0, latitude) * worldSize;
-
-    const double bearingRadians = degreesToRadiansIfNeeded(params.bearing);
-    const double pitchRadians = degreesToRadiansIfNeeded(params.pitch);
-
-    const double dx = pointX - cameraX;
-    const double dy = pointY - cameraY;
-
-    const double rotatedX = dx * std::cos(bearingRadians) - dy * std::sin(bearingRadians);
-    const double rotatedY = dx * std::sin(bearingRadians) + dy * std::cos(bearingRadians);
-
-    const double pitchedY = rotatedY * std::cos(pitchRadians) - altitudePixels * std::sin(pitchRadians);
-
-    return ScreenPoint{
-            .x = 2.0 * rotatedX / params.width,
-            .y = -2.0 * pitchedY / params.height,
-    };
-}
-
-ScreenPoint projectMetersOffsetToNdc(
-        double originLongitude,
-        double originLatitude,
-        double originAltitudeMeters,
-        LocalMeters offsetMeters,
-        const mbgl::style::CustomLayerRenderParameters& params
-) {
-    const double worldSize = kTileSize * std::pow(2.0, params.zoom);
-    const double worldUnitsPerMeter = metersToMercatorUnits(1.0, originLatitude) * worldSize;
-
-    const double cameraX = longitudeToMercatorX(params.longitude) * worldSize;
-    const double cameraY = latitudeToMercatorY(params.latitude) * worldSize;
-
-    const double originX = longitudeToMercatorX(originLongitude) * worldSize;
-    const double originY = latitudeToMercatorY(originLatitude) * worldSize;
-
-    const double pointX = originX + offsetMeters.east * worldUnitsPerMeter;
-    const double pointY = originY - offsetMeters.north * worldUnitsPerMeter;
-    const double pointAltitudePixels = (originAltitudeMeters + offsetMeters.up) * worldUnitsPerMeter;
-
-    const double bearingRadians = degreesToRadiansIfNeeded(params.bearing);
-    const double pitchRadians = degreesToRadiansIfNeeded(params.pitch);
-
-    const double dx = pointX - cameraX;
-    const double dy = pointY - cameraY;
-
-    const double rotatedX = dx * std::cos(bearingRadians) - dy * std::sin(bearingRadians);
-    const double rotatedY = dx * std::sin(bearingRadians) + dy * std::cos(bearingRadians);
-
-    const double pitchedY = rotatedY * std::cos(pitchRadians) - pointAltitudePixels * std::sin(pitchRadians);
-
-    return ScreenPoint{
-            .x = 2.0 * rotatedX / params.width,
-            .y = -2.0 * pitchedY / params.height,
-    };
 }
 
 }  // namespace custom_map_layers::geo
--- app/src/main/cpp/lib/geo/WebMercator.hpp
+++ app/src/main/cpp/lib/geo/WebMercator.hpp
@@ -1,13 +1,6 @@
 #pragma once
 
-#include "custom_map_layers/maplibre/custom_layer_host.hpp"
-
 namespace custom_map_layers::geo {
-
-struct ScreenPoint {
-    double x;
-    double y;
-};
 
 struct LocalMeters {
     double east;
@@ -18,20 +11,5 @@ struct LocalMeters {
 double longitudeToMercatorX(double longitude);
 double latitudeToMercatorY(double latitude);
 double metersToMercatorUnits(double meters, double latitude);
-
-ScreenPoint projectToNdc(
-        double longitude,
-        double latitude,
-        double altitudeMeters,
-        const mbgl::style::CustomLayerRenderParameters& parameters
-);
-
-ScreenPoint projectMetersOffsetToNdc(
-        double originLongitude,
-        double originLatitude,
-        double originAltitudeMeters,
-        LocalMeters offsetMeters,
-        const mbgl::style::CustomLayerRenderParameters& parameters
-);
 
 }  // namespace custom_map_layers::geo
